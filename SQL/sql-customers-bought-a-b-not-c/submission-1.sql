37b9921ced773ea61a2515f1c3dd347f3a623dbf
Meera <46873861+meera-ramesh19@users.noreply.github.com>
2026-05-23 15:21:07 -0400

    Delete SQL/sql-customers-bought-a-b-not-c/submission-1.sql

--- SQL/sql-customers-bought-a-b-not-c/submission-1.sql
+++
@@ -1,8 +0,0 @@
--- Write your query below
-select c.customer_id, c.customer_name
-from customers c join orders o on c.customer_id = o.customer_id
-group by c.customer_name,c.customer_id
-HAVING SUM(CASE WHEN o.product_name = 'A' THEN 1 ELSE 0 END) > 0
-   and SUM(CASE WHEN o.product_name = 'B' THEN 1 ELSE 0 END) > 0
-   AND SUM(CASE WHEN o.product_name = 'C' THEN 1 ELSE 0 END) = 0
-order by c.customer_name;
